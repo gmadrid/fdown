79b1d09c68976dd11a2e8dcb8a8548af6369440a
George Madrid <gmadrid@gmail.com>
2016-09-13 11:57:49 -0400

    Everything seems to work except saving.

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,11 @@ mod result;
 use feedly::{Feedly};
 use generated::EntryDetail;
 use hyper::Client;
+use std::ffi::OsStr;
 use std::fs::File;
 use std::io::Read;
 use std::io::Write;
+use std::path::{Path,PathBuf};
 use std::process::Command;
 
 fn open_url(url: &String) -> result::Result<()> {
@@ -32,13 +34,47 @@ fn download_image(url: &String) -> result::Result<(Vec<u8>)> {
   Ok(buf)
 }
 
-    // let client = Client::new();
-    // let url = format!("http://cloud.feedly.com/v3/streams/ids?streamId={}", self.saved_feed());
-    // let response = try!(client.get(&url)
-    //     .header(self.auth_header())
-    //     .send());
-    // let response : StreamsIdsResponse = try!(serde_json::from_reader(response));
+fn add_number_suffix(stem: &OsStr, num: usize) -> result::Result<String> {
+  if let Some(stem) = stem.to_str() {
+    return Ok(format!("{}_({})", stem, num))
+  }
+  Err(result::FdownError::BadFormat(format!("cannot append number to stem: {}", stem.to_string_lossy())))
+}
 
+fn file_new_path(path: PathBuf) -> result::Result<PathBuf> {
+  let mut num = 0;
+  let extension = path.extension().unwrap_or(OsStr::new(""));
+  if let Some(stem) = path.file_stem() {
+    loop {
+      num += 1;
+      let new_name = try!(add_number_suffix(stem, num));
+      let new_path = path.with_file_name(new_name).with_extension(extension);
+      if !new_path.exists() {
+        return Ok(new_path);
+      }
+    }
+  }
+  Err(result::FdownError::BadFormat(format!("unable to find file stem in url: {}", path.to_string_lossy())))
+}
+
+fn filepath_for_url(url: &String) -> result::Result<PathBuf> {
+  // Get the likely filename from the url.
+  // TODO: check to ensure that the filename has an extension.
+  if let Some(slash_index) = url.rfind('/') {
+    let filename = Path::new(&url[slash_index + 1..]);
+    let path = Path::new("/Users/gmadrid/Dropbox/ATestDir").join(filename);
+    if path.exists() {
+      return file_new_path(path);
+    }
+    return Ok(path);
+  }
+  Err(result::FdownError::BadFormat(format!("unable to extract filename from url: {}", url)))  
+}
+
+fn unsave_entry(entry: &EntryDetail) -> result::Result<()> {
+  // TODO: do this.
+  Ok(())
+}
 
 fn process_entry(entry: &EntryDetail) -> result::Result<()> {
   // For each, 
@@ -50,8 +86,10 @@ fn process_entry(entry: &EntryDetail) -> result::Result<()> {
   if let Some(url) = Feedly::extract_image_url(entry) {
     let url = Feedly::tumblr_filter(url);
     let image_bytes = try!(download_image(&url));
-    let mut foo = try!(File::create("/tmp/myimage.jpg"));
-    foo.write(&image_bytes);
+    let path = try!(filepath_for_url(&url));
+    let mut foo = try!(File::create(path));
+    try!(foo.write(&image_bytes));
+    try!(unsave_entry(entry));
   }
   Ok(())
 }
@@ -63,15 +101,15 @@ fn main() {
   let userid = config.required_string("userid");
   let token = config.required_string("token");
   let feedly = feedly::new(userid, token);
-  // let ids = feedly.saved_entry_ids().unwrap();
+  let ids = feedly.saved_entry_ids().unwrap();
 
-  let ids = vec!(
-    "cfBX1FTyBgWMD47LB+mDBO8xvSRPMYW+Yf70hpffjGI=_156bd0d2737:20fa60f:45cbc242".to_string(),
-    "cfBX1FTyBgWMD47LB+mDBO8xvSRPMYW+Yf70hpffjGI=_1570b3456ad:b7c503e:e3157ec0".to_string(),
-    "cfBX1FTyBgWMD47LB+mDBO8xvSRPMYW+Yf70hpffjGI=_156b6ba88b8:767b5a:e0992bbc".to_string()
-    );
+  // let ids = vec!(
+  //   "cfBX1FTyBgWMD47LB+mDBO8xvSRPMYW+Yf70hpffjGI=_156bd0d2737:20fa60f:45cbc242".to_string(),
+  //   "cfBX1FTyBgWMD47LB+mDBO8xvSRPMYW+Yf70hpffjGI=_1570b3456ad:b7c503e:e3157ec0".to_string(),
+  //   "cfBX1FTyBgWMD47LB+mDBO8xvSRPMYW+Yf70hpffjGI=_156b6ba88b8:767b5a:e0992bbc".to_string()
+  //   );
   let details = feedly.detail_for_entries(ids).unwrap();
   for detail in details.iter() {
-    process_entry(detail);
+    process_entry(detail).map_err(|err| println!("{:?}", err));
   }
 }
